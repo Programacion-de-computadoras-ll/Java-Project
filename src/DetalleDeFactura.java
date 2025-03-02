9022be542e0a26d44e31008b6b3ee08e33ea9ee9
Ninfarno <miluismendoza1@gmail.com>
2025-03-01 19:21:05 -0600

    Update DetalleDeFactura.java

--- src/DetalleDeFactura.java
+++ src/DetalleDeFactura.java
@@ -5,25 +5,21 @@ public class DetalleDeFactura {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingrese el nombre de la factura:");
-        String nombreFactura = scanner.nextLine();
- System.out.println("Ingrese el precio del primer producto:");
-        double precio1 = scanner.nextDouble();
+             String nombreFactura = scanner.nextLine();
+        System.out.println("Ingrese el precio del primer producto:");
+             double precio1 = scanner.nextDouble();
 
         System.out.println("Ingrese el precio del segundo producto:");
-        double precio2 = scanner.nextDouble();
+             double precio2 = scanner.nextDouble();
 
-
-        double totalBruto = precio1 + precio2;
-
-        double impuesto = totalBruto * 0.19;
-
-
-        double totalNeto = totalBruto + impuesto;
+             double totalBruto = precio1 + precio2;
+             double impuesto = totalBruto * 0.19;
+             double totalNeto = totalBruto + impuesto;
 
         String detalle = "Nombre de la factura: " + nombreFactura + "\n" +
                          "Monto total bruto: " + totalBruto + "\n" +
                          "Impuesto: " + impuesto + "\n" +
-                         "Monto total neto: " + totalNeto;
+                         "Monto total neto: " + totalNeto + "\n";
 
         System.out.println(detalle);
     }
